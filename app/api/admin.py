92d35cfb02acc48b1f92e52c314ee4e284fd8e05
Daniel Herrmann <ministry@scopal-affairs.com>
2019-04-02 21:23:51 +0200

    refactor(backend) switched to flask and cleaned up everything - tabula rasa

--- app/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rest_framework.authtoken.admin import TokenAdmin
-
-from .models import Bucketlist
-
-TokenAdmin.raw_id_fields = ('user',)
-
-# Register your models here.
-admin.site.register(Bucketlist)
