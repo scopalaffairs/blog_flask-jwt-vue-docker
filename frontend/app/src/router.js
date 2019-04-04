529a3a87c546ca2345cc9aa8ca1850a694c8896e
Daniel Herrmann <ministry@scopal-affairs.com>
2019-04-04 23:41:37 +0200

    feat(login) autoAuth testwise integrated

--- frontend/app/src/router.js
+++ frontend/app/src/router.js
@@ -1,30 +1,73 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import axiosAuth from '@/api/axios-auth'
 
 Vue.use(Router);
 
-export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import(/* webpackChunkName: "login" */ './views/Login.vue')
-        },
-        {
-            path: '/logout',
-            name: 'logout',
-            component: () => import(/* webpackChunkName: "login" */ './views/Logout.vue')
-        },
-        {
-            path: '/blog',
-            name: 'blog',
-            component: () => import(/* webpackChunkName: "blog" */ './views/Blog.vue')
-        }
-    ]
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "login" */ './views/Login.vue')
+    },
+    {
+        path: '/logout',
+        name: 'logout',
+        component: () => import(/* webpackChunkName: "login" */ './views/Logout.vue')
+    },
+    {
+        path: '/blog',
+        name: 'blog',
+        component: () => import(/* webpackChunkName: "blog" */ './views/Blog.vue')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        meta: {requiresAuth: true},
+        component: () => import(/* webpackChunkName: "blog" */ './views/templates/admin/Dashboard.vue'),
+    },
+];
+
+const router = new Router({
+    routes: routes,
 });
 
+router.beforeEach((to, from, next) => {
+    let token = localStorage.getItem('token');
+    let requireAuth = to.matched.some(record => record.meta.requiresAuth);
+
+    if (!requireAuth) {
+        next();
+    }
+
+    if (requireAuth && !token) {
+        next('/login');
+    }
+
+    if (to.path === '/login') {
+        if (token) {
+            axiosAuth.post('/decode_token').then(() => {
+                next('/dashboard');
+            }).catch(() => {
+                next();
+            });
+        } else {
+            next();
+        }
+    }
+
+    if (requireAuth && token) {
+        axiosAuth.post('/decode_token').then(() => {
+            next();
+        }).catch(() => {
+            next('/login');
+        })
+    }
+});
+
+export default router;
